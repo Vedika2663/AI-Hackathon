7c476c90b81b14d4bb4ce2fe2c91b2f3f662ce1d
Vedika Chowdhary <vedikachowdhary2003@gmail.com>
2024-01-25 20:07:22 +0530

    added bottom  bar

---
+++ src/BottomBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./homePage.css";
+function Bottombar(){
+    return (
+        <>
+        <div className="effects"></div>
+        <div className="overeffects"></div>
+        <div className="effectsRight"></div>
+        <div className="overeffectsRight"></div>
+        </>
+    )
+}
+export default Bottombar;
